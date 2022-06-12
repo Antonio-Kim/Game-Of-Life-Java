b040cf9f48870f4ef7e0a8b568855de7d4f206f7
Antonio Kim <antoniok@sfu.ca>
2022-06-11 21:56:04 -0700

    draw horizontal and vertical lines to mimic grid

--- application/GamePanel.java
+++ application/GamePanel.java
@@ -4,6 +4,14 @@ import javax.swing.JPanel;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
+    private final static int CELLSIZE = 50;
+    private final static Color backgroundColor = Color.BLACK;
+    private final static Color foregroundColor = Color.GREEN;
+    private final static Color gridColor = Color.GRAY;
+
+    private int topBottomMargin;
+    private int leftRightMargin;
+
     public GamePanel() {
         setBackground(Color.RED);
     }
@@ -15,7 +23,25 @@ public class GamePanel extends JPanel {
         int width = getWidth();
         int height = getHeight();
 
-        g2.setColor(Color.GREEN);
-        g2.fillRect(0,0, width, height);
+        leftRightMargin = ((width % CELLSIZE) + 50) / 2;
+        topBottomMargin = ((height % CELLSIZE) + 50) / 2;
+
+        g2.setColor(backgroundColor);
+        g2.fillRect(0, 0, width, height);
+
+        drawGrid(g2, width, height);
+    }
+
+    private void drawGrid(Graphics2D g2, int width, int height) {
+        g2.setColor(gridColor);
+
+        for (int i = leftRightMargin; i <= width - leftRightMargin; i += CELLSIZE) {
+            g2.drawLine(i, topBottomMargin, i, height - topBottomMargin);
+        }
+
+        for (int j = topBottomMargin; j <= height - topBottomMargin; j += CELLSIZE) {
+            g2.drawLine(leftRightMargin, j, width-leftRightMargin, j);
+        }
+
     }
 }
